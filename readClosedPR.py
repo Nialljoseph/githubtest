b975feadf7c65b7aa6ab79527d61ce1656d7128c
Niall Joseph <njoseph21@csus.org>
2020-08-12 14:00:09 -0700

    Cleaned up files and added GitHub API

--- readClosedPR.py
+++
@@ -1,8 +0,0 @@
-from github import Github
-
-
-g = Github("c3beda8673b9539d597c37fef28a32cc7e076d42")
-repo = g.get_repo("Nialljoseph/GithubTest")
-prlist = repo.get_pulls(state='closed', base='master')
-for pr in prlist:
-    print(pr.number)
